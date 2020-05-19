1ff676a61a0bf0c98db00004f0537d2e16a177a5
PieKnu <pierreknudsen123@gmail.com>
2020-05-19 13:22:10 +0200

    Updated to the latest version & implemented the teammember pending approvals

--- src/app/service/team-list.service.ts
+++ src/app/service/team-list.service.ts
@@ -1,11 +1,24 @@
 import { Injectable } from '@angular/core';
 import { User } from '../models/user';
 import { Observable } from 'rxjs';
+import { Team } from '../models/team';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TeamListService {
 
-  constructor() { }
+   teams: Team[] = [];
+
+  constructor() { 
+    this.teams.push(new Team("Team in teamlist.service!"));
+  }
+
+  public getTeamList(): Observable<Team[]> { //
+    const arrayObservable = new Observable<Team[]>(observer => {
+          observer.next(this.teams);
+    });
+    return arrayObservable;
+  }
+
 }
