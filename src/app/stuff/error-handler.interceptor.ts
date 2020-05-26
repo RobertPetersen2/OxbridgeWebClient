fcb43fa8b80065924b3eac7f7e9b3090daeb2f0f
PieKnu <pierreknudsen123@gmail.com>
2020-05-26 10:16:45 +0200

    Added HttpErrorInterceptor that will perform a logout of the currentUser if the node/backend returns a 401 error code

---
+++ src/app/stuff/error-handler.interceptor.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+import { AuthenticationService } from '../service/authentication.service';
+
+@Injectable()
+export class ErrorHandlerInterceptor implements HttpInterceptor {
+    constructor(private authenticationService: AuthenticationService) {}
+
+    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        return next.handle(request).pipe(catchError(err => {
+            if (err.status === 401) {
+                // The logged in user will be logged out if 401 response is returned from the api
+                this.authenticationService.logout();
+                location.reload(true);
+            }
+            
+            const error = err.error.message || err.statusText;
+            return throwError(error);
+        }))
+    }
+}
