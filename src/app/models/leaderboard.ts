6efbd380daf422994898c27b40d02048af768b4b
PieKnu <pierreknudsen123@gmail.com>
2020-06-10 11:06:48 +0200

    Leaderboard service

---
+++ src/app/models/leaderboard.ts
@@ -0,0 +1,3 @@
+export class Leaderboard {
+    public teamName: string;
+}
--- src/app/service/leaderboard.service.ts
+++ src/app/service/leaderboard.service.ts
@@ -1,9 +1,27 @@
-import { Injectable } from '@angular/core';
+import { Injectable, EventEmitter } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { Leaderboard } from '../models/leaderboard';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LeaderboardService {
 
-  constructor() { }
+  private teamList: BehaviorSubject<Leaderboard[]>;
+
+  constructor(private http:HttpClient) { 
+
+    this.teamList = new BehaviorSubject<Leaderboard[]>([]);
+
+  }
+
+  public getLeadereboard(): Observable<Leaderboard[]>{
+    const teamList = this.http.get<Leaderboard[]>('http://148.251.122.228:3000/races/leaderboard');
+    teamList.subscribe((response: Leaderboard[]) => {
+      this.teamList.next(response);
+    });
+    return this.teamList;
+  }
+
 }
